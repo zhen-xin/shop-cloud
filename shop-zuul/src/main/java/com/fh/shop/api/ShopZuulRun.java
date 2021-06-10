204b74a29f9ec174f645918fda24e16e26ebfb83
zhen-xin <w319257383@163.com>
2021-06-10 18:37:28 +0800

    dev分支上传

--- shop-zuul/src/main/java/com/fh/shop/api/ShopZuulRun.java
+++ shop-zuul/src/main/java/com/fh/shop/api/ShopZuulRun.java
@@ -10,6 +10,7 @@ public class ShopZuulRun {
 
     public static void main(String[] args) {
         System.out.println("更新");
+        System.out.println("dev上传");
         SpringApplication.run(ShopZuulRun.class, args);
     }
 
